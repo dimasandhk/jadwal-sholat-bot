231165c9afb929ef511c91d954ce0a945a5ffd3f
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-06-21 16:53:05 +0700

    Move Fetch data to a class from seperate file

--- src/api/ApiService.ts
+++ src/api/ApiService.ts
@@ -1,4 +1,31 @@
+import axios from "axios";
+import Discord from "discord.js";
+
+// Utils
+import urlMaker from "../utils/urlMaker";
+import { searchEndpoint } from "../utils/var";
+import makeEmbed from "../utils/embed";
+
 export default class {
-	private static getLocationId() {}
-	public static async fetchJadwal() {}
+	private static async getLocationId(location: string) {
+		const response = await (await axios.get(`${searchEndpoint}${location}`)).data;
+		if (!response.status) return "Kota Tidak Ketemu";
+
+		return response.data[0].id;
+	}
+	public static async fetchJadwal(lokasi: string = "Jakarta", msg: Discord.Message) {
+		let id: string = "1301";
+
+		const getId = await this.getLocationId(lokasi.trim());
+		if (getId == "Kota Tidak Ketemu") return msg.reply("Kota Tidak Ketemu");
+		id = getId;
+
+		const response = await (await axios.get(urlMaker(id))).data;
+
+		const jadwal = response.data.jadwal;
+		const judul = `${response.data.lokasi} (${jadwal.tanggal})`;
+		const arrOfJadwal = Object.entries(Object.entries(jadwal));
+
+		msg.channel.send(makeEmbed(judul, arrOfJadwal));
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import Discord from "discord.js";
-import axios from "axios";
-
 const client = new Discord.Client();
 
-// Utils
-import urlMaker from "./utils/urlMaker";
-import { searchEndpoint } from "./utils/var";
-import makeEmbed from "./utils/embed";
+import ApiService from "./api/ApiService";
 
 // Dotenv Config
 import dotenv from "dotenv";
@@ -16,37 +11,14 @@ client.on("ready", (): void => console.log(`Logged in as ${client.user?.tag}`));
 
 client.on("message", (msg) => {
 	const qValue: string = msg.content.split("!jadwalsholat")[1];
-	const getLocationId = async (location: string) => {
-		const response = await (await axios.get(`${searchEndpoint}${location}`)).data;
-		if (!response.status) return "Kota Tidak Ketemu";
-
-		return response.data[0].id;
-	};
-
-	const fetchJadwal = async (lokasi: string = "Jakarta") => {
-		let id: string = "1301";
-		const getId = await getLocationId(lokasi.trim());
-		if (getId == "Kota Tidak Ketemu") {
-			return msg.reply("Kota Tidak Ketemu");
-		}
-
-		id = getId;
-		const response = await (await axios.get(urlMaker(id))).data;
-
-		const jadwal = response.data.jadwal;
-		const judul = `${response.data.lokasi} (${jadwal.tanggal})`;
-		const arrOfJadwal = Object.entries(Object.entries(jadwal));
-
-		msg.channel.send(makeEmbed(judul, arrOfJadwal));
-	};
 
 	if (msg.content.includes("!jadwalsholat")) {
 		if (!qValue) {
 			msg.reply("Kamu bisa lihat list kota dengan ketik '!listkota'");
-			return fetchJadwal();
+			return ApiService.fetchJadwal("Jakarta", msg);
 		}
 
-		return fetchJadwal(qValue);
+		return ApiService.fetchJadwal(qValue, msg);
 	} else if (msg.content == "!listkota") {
 		msg.reply(
 			"Kamu bisa lihat di link ini:\nhttps://gist.github.com/dimasandhk/6f9e159176a75b99087a667727208cf5"
